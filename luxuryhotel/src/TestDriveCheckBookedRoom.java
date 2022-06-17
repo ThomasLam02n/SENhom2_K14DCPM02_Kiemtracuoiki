d2f9266b38216c1bcb2fc6e061207531429f693b
phongtruong2106 <thpgamer42@gmail.com>
2022-06-18 01:10:42 +0700

    up checkBookedRoom , don't confirm bookedRoom

---
+++ luxuryhotel/src/TestDriveCheckBookedRoom.java
@@ -0,0 +1,6 @@
+public class TestDriveCheckBookedRoom {
+    public static void main(String[] args) {
+        CheckBookedRoomController checkBookedRoomController = new CheckBookedRoomController();
+        checkBookedRoomController.SearchBill(123);
+    }
+}
