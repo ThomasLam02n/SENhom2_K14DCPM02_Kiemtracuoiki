30709e8a7bc8aa9981d441e2a5897ed1ed352e19
Thach <hoangthachnao@gmail.com>
2022-06-18 20:37:30 +0700

    up code canelBill

---
+++ luxuryhotel/src/CanelBillController.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CanelBillController {
+    static Scanner input = new Scanner(System.in);
+    
+    Room room;
+    public void CanelBill(){
+        input.nextLine();
+            System.out.println("Enter the item code to delete:");
+            String deleteCode = input.nextLine();
+            Room room = null;
+            // for (Room room2 : danhSach) {
+                // if(hangHoa.getMaHang().equalsIgnoreCase(deleteCode)){
+                //     hangHoa1 =  hangHoa;
+                //     break;
+                // }
+            }
+            // if(hangHoa1 != null){
+            //     danhSach.remove(hangHoa1);
+            //     System.out.println("Item has been deleted");
+            // }else{
+            //     System.out.println("The item code to be deleted is not found in the list ");
+            // }
+    // }
+}
