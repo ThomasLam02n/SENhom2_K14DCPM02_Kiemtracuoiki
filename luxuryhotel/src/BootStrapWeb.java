3e9b243df08a4c0b95bad8df5b3e084ca0789ab0
phongtruong2106 <thpgamer42@gmail.com>
2022-06-18 20:38:18 +0700

    up lại

--- luxuryhotel/src/BootStrapWeb.java
+++ luxuryhotel/src/BootStrapWeb.java
@@ -1,70 +1,34 @@
+
+import java.util.Date;
 import java.util.Scanner;
 
+import javax.xml.crypto.Data;
+
 public class BootStrapWeb {
-    public static void main(String[] args) throws InterruptedException {
 
-
-        Account account = new Account();
-        NewAccountController newAccountController = new NewAccountController(account);
-       NewAccountUI newAccountUI = new NewAccountUI(newAccountController);
-
-       //login 
-
-       LoginAccountController loginAccountController = new LoginAccountController(account);
-       LoginAccountUI loginAccountUI = new LoginAccountUI(loginAccountController);
-
-       Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcom to the LuxuryHotel System");
+    
+    public static void main(String[] args) {
         
-        while(true){
-            Thread.sleep(1000);
-            displayOption(newAccountController);
-            String prompt = getPrompt(newAccountController);
-            System.out.println(prompt);
+        System.out.println("Welcom to the LuxuryHotel System");
 
-            String rep = scanner.nextLine();
-
-            String resCMD;
-            if(rep.toUpperCase().equals(Actions.CA.toString())){
-                resCMD = newAccountUI.handleCommand(rep);
-                System.out.println(resCMD);
-
-                if(resCMD != null && !resCMD.equals("Unkown command.")){
-                    newAccountUI.handleInputs();
-                }
-            } else if(rep.toUpperCase().equals(Actions.LA.toString()) 
-            || rep.toUpperCase().equals(Actions.LO.toString())){
-                resCMD = loginAccountUI.handleCommands(rep);
-                if(resCMD != null && !resCMD.equals("Unkown command")){
-                    loginAccountUI.handleInputs();
-                }
-            }
+        while (true){
             
         }
     }
 
-    public static void displayOption(NewAccountController newAccountController){
-        System.out.println("__________________CRS MENU________________");
-
-        //check
-
-        String str = "";
-        if(!newAccountController.getAccount().checkLoggedIn()){
-            str = "Enter one of the command in the brackets: \n " +
-            "[CA] Create Account\n" + 
-            "[LA] Login"; 
-            System.out.println(str);
-        }else{
-            System.out.println("Enter on of the commands in bracket:\n " + 
-                    "[LO] logout");
-        }
-
+    public void displayOption(){
+       
     }
-    public static String getPrompt(NewAccountController newAccountController){
-        if(!newAccountController.getAccount().checkLoggedIn()){
-            return "";
-        }
 
-        return "LOGGED IN AS #" + newAccountController.getAccount().getUsername();
+    public void handleInputs(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Location: ");
+        String location = scanner.nextLine();
+        System.out.println("Check in Date:");
+        String cid = scanner.nextLine();
+        System.out.println("Check out Date: ");
+        String cod = scanner.nextLine();
+        System.out.println("Amount of people:");
+        int aop = scanner.nextInt();
     }
 }
