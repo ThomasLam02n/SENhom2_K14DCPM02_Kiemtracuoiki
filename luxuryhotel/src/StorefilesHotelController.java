693854cc017876d98765b9f1f7c551fb18ce257c
Thach <hoangthachnao@gmail.com>
2022-06-22 20:06:53 +0700

    up code tong hop

--- luxuryhotel/src/StorefilesHotelController.java
+++ luxuryhotel/src/StorefilesHotelController.java
@@ -20,13 +20,13 @@ public class StorefilesHotelController {
     public JsonArray getMemory() {
         return memory;
     }
-    public void update(String name, String address,int room_quantity, String evaluation, String utilities){
+    public void update(String name, String address,int room_quantity, JsonArray evaluation, String utilities){
         JsonObject jsonObject = new JsonObject();
 
         jsonObject.addProperty("name", name);
         jsonObject.addProperty("address", address);
         jsonObject.addProperty("RoomQuantity", room_quantity);
-        jsonObject.addProperty("Evaluation", evaluation);
+        jsonObject.add("Evaluation", evaluation);
         jsonObject.addProperty("utilities", utilities);
         this.memory.add(jsonObject);
     }
